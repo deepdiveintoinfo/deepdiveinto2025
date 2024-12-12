b339c9d08953dde3ce81c207b8a720c5295442e6
Kevin Baugh <loraxx753@gmail.com>
2024-12-11 19:27:35 -0600

    Word count works for content source.

--- src/components/Pages/project2025/section/chapter/ContentSources/component.tsx
+++ src/components/Pages/project2025/section/chapter/ContentSources/component.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Ticker } from "@/components/ThirdParty/Animata/Ticker";
 import { Card, CardHeader, CardFooter, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ThirdParty/ShadCn";
@@ -15,7 +15,23 @@ export const ContentSources = ({ chapter }: ContentSourcesProps) => {
     const [dropdowLabelValue, setDropdowLabelValue] = useState("Original");
     const [CurrentVersion, setCurrentVersion] = useState<MdxComponent | undefined>(chapter?.versions?.original);
 
+    const [wordCount, setWordCount] = useState(chapter.metadata.wordcount)
+
+    useEffect(() => {
+        const versions = chapter?.versions;
+        const v = versions && Object.keys(versions).includes(dropdowLabelValue.toLowerCase());
+
+        setDropdowLabelValue(v ? dropdowLabelValue : 'Original');
+        setCurrentVersion(v ? (versions[dropdowLabelValue.toLowerCase() as keyof typeof versions] as MdxComponent) : versions?.original || undefined);
+        setWordCount(document.querySelector('section')?.textContent?.split(/[\W]+/g).length || 0)
+
+    }, [ chapter, dropdowLabelValue ])
+    useEffect(() => {
+        setWordCount(document.querySelector('section')?.textContent?.split(/[\W]+/g).length || 0)
+    },[dropdowLabelValue])
+
     if(!CurrentVersion) return <p>Loading...</p>
+
 
 
     return (
@@ -23,7 +39,7 @@ export const ContentSources = ({ chapter }: ContentSourcesProps) => {
         <div className="flex flex-col lg:flex-row gap-4 my-4">
             <Card>
                 <CardHeader className="flex justify-center mb-0 pb-0 gap-3">
-                    <p className="flex justify-center text-4xl bold m-0 p-0"><Ticker value={chapter.metadata.wordcount.toLocaleString()} /></p>
+                    <p className="flex justify-center text-4xl bold m-0 p-0"><Ticker value={wordCount.toLocaleString()} /></p>
                 </CardHeader>
                 <CardFooter className="py-0 mt-3 justify-center font-bold">
                     Words
@@ -31,7 +47,7 @@ export const ContentSources = ({ chapter }: ContentSourcesProps) => {
             </Card>
         </div>
         <div className="relative bg-white border border-gray-500/10 p-4">
-            <div className="absolute top-4 right-4 flex flex-col md:flex-row gap-2">
+            <div className="md:absolute top-4 right-4 flex gap-2">
                 <p className='p-0 m-0'>Readability: </p>
                 <DropdownMenu>
                     <DropdownMenuTrigger className='flex items-center gap-1'>{dropdowLabelValue} <ChevronDownIcon className='w-4 h-4' /></DropdownMenuTrigger>
@@ -39,13 +55,15 @@ export const ContentSources = ({ chapter }: ContentSourcesProps) => {
                         { chapter?.versions?.basic && <DropdownMenuItem onClick={() => {setDropdowLabelValue('Basic'); setCurrentVersion(chapter?.versions?.basic) }}>Basic</DropdownMenuItem> }
                         { chapter?.versions?.simple && <DropdownMenuItem onClick={() => {setDropdowLabelValue('Simple'); setCurrentVersion(chapter?.versions?.simple) }}>Simple</DropdownMenuItem> }
                         { chapter?.versions?.clear && <DropdownMenuItem onClick={() => {setDropdowLabelValue('Clear'); setCurrentVersion(chapter?.versions?.clear) }}>Clear</DropdownMenuItem> }
-                        { chapter?.versions?.original && <DropdownMenuItem onClick={() => {setDropdowLabelValue('Source'); setCurrentVersion(chapter?.versions?.original) }}>Original</DropdownMenuItem> }
+                        { chapter?.versions?.original && <DropdownMenuItem onClick={() => {setDropdowLabelValue('Original'); setCurrentVersion(chapter?.versions?.original) }}>Original</DropdownMenuItem> }
                     </DropdownMenuContent>
                 </DropdownMenu>
             </div>
             <h1>{chapter.title}</h1>
             <p><i>{chapter.metadata.authors.map(a => a.name).join(', ')}</i></p>
-            {CurrentVersion && CurrentVersion as unknown as React.ReactNode}
+            <section>
+                {CurrentVersion && CurrentVersion as unknown as React.ReactNode}
+            </section>
         </div>
         </>
     );
